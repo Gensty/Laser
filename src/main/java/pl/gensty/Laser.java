56bdbe1523e915a13955d84f22a68b9593c1eab6
Michał Przymorski <michal.przymorski@gmail.com>
2024-06-19 17:55:38 +0200

    Create project

--- src/main/java/pl/gensty/Laser.java
+++ src/main/java/pl/gensty/Laser.java
@@ -57,6 +57,10 @@ public class Laser extends JFrame {
         add(scrollPane, BorderLayout.CENTER);
     }
 
+    public void laser() {
+
+    }
+
     private void setFilenames() {
         String catalogPath = catalogPathField.getText();
         String excelPath = excelPathField.getText();
